8a75c02feb539441d13eb559249a0b4cb8abe626
Jean Gal <j.c.r.gal@student.tudelft.nl>
2021-01-05 15:24:38 +0100

    Start side menu for storage

--- lib/pages/Storage.dart
+++ lib/pages/Storage.dart
@@ -15,6 +15,33 @@ class Storage extends StatelessWidget {
           preferredSize: const Size.fromHeight(60),
           child: StorageAppBar(),
         ),
+        drawer: new Drawer(
+          child: ListView(
+            children: const <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                ),
+                child: Text(
+                  'Drawer Header',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text("Materias Primas"),
+              ),
+              ListTile(
+                title: Text("Trabajo en Processo"),
+              ),
+              ListTile(
+                title: Text("Producto Terminado"),
+              ),
+            ],
+          ),
+        ),
         body: Container(),
       ),
     );
