8c67fb44bb4fb2c84fa1fb5450777e04ff225f50
Jean Gal <j.c.r.gal@student.tudelft.nl>
2021-01-11 04:54:46 +0100

    Added new icon + positioning of elements in transaction description.

--- lib/pages/Transactions.dart
+++ lib/pages/Transactions.dart
@@ -2,6 +2,7 @@ import 'package:chankuap_flutter/app_bars/transaction_app_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../app_icons.dart';
 import '../drawer_header.dart';
 
 class Transactions extends StatefulWidget {
@@ -60,11 +61,11 @@ class _TransactionState extends State<Transactions> {
                   children: <Widget>[
                     Expanded(
                       flex: 2,
-                      child: Icon(Icons.add),
+                      child: Icon(AppIcons.entry, color: Color(0xff073B3A)),
                     ),
                     Expanded(
                       flex: 5,
-                      child: Text("Entrada de Mercaderia", style: TextStyle(color: Colors.teal[900], fontSize: 16)),
+                      child: Text("Entrada de Mercaderia", style: TextStyle(color: Color(0xff073B3A), fontSize: 16)),
                     ),
                     Expanded(
                       flex: 3,
@@ -79,15 +80,15 @@ class _TransactionState extends State<Transactions> {
                         },
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(18.0),
-                            side: BorderSide(color: Colors.teal[900])
+                            side: BorderSide(color: Color(0xff073B3A))
                         ),
                         child: Row(
                           children: <Widget>[
                             Expanded(
                               flex: 6,
-                              child: Text("Anadir", style: TextStyle(color: Colors.teal[900]))
+                              child: Text("Anadir", style: TextStyle(color: Color(0xff073B3A)))
                             ),
-                            Icon(Icons.add, color: Colors.teal[900]),
+                            Icon(Icons.add, color: Color(0xff073B3A)),
                           ],
                         ),
                       ),
@@ -165,24 +166,77 @@ class _TransactionState extends State<Transactions> {
   }
 
   Widget _buildPopupDialog(BuildContext context) {
-    return new AlertDialog(
-      title: const Text('Popup example'),
-      content: new Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text("Hello"),
+    return new Dialog(
+      backgroundColor: Color(0xffEFEFEF),
+      insetPadding: EdgeInsets.fromLTRB(20, 100, 20, 100),
+      child: new Stack(
+        children: [
+          Positioned(child: Text("Description de transaccion"), top: 20, left: 80),
+          Positioned(child: Text("Fecha"), top: 60, left: 70),
+          Positioned(child: Text("Fecha"), top: 60, right: 70),
+          Positioned(child: Container(
+            height: 30,
+            width: 130,
+            color: Colors.white,
+            child: Align(
+              alignment: Alignment.center,
+              child: Text("12/10/20"),
+            )
+          ),
+          top: 90, left: 20),
+          Positioned(child: Container(
+            height: 30,
+            width: 130,
+            color: Colors.white,
+            child: Align(
+              alignment: Alignment.center,
+              child: Text("13/10/20"),
+            )
+          ),
+          top: 90, right: 20),
+          Positioned(child: Text("Cliente"), top: 140, left: 140),
+          Positioned(child: Container(
+              height: 30,
+              width: 280,
+              color: Colors.white,
+              child: Align(
+                alignment: Alignment.center,
+                child: Text("Pooti Somik Chios"),
+              )
+          ), top: 170, left: 20),
+          Positioned(child: Text("Ciudad / Communidad"), top: 220, left: 20),
+          Positioned(child: Text("Compania Aéra"), top: 220, right: 40),
+          Positioned(child: Container(
+              height: 30,
+              width: 130,
+              color: Colors.white,
+              child: Align(
+                alignment: Alignment.center,
+                child: Text("Macas"),
+              )
+          ), top: 250, left: 20),
+          Positioned(child: Container(
+              height: 30,
+              width: 130,
+              color: Colors.white,
+              child: Align(
+                alignment: Alignment.center,
+                child: Text("KLM"),
+              )
+          ), top: 250, right: 20),
+          Positioned(child: Text("Codigo del Productor"), top: 300, left: 100),
+          Positioned(child: Container(
+              height: 30,
+              width: 280,
+              color: Colors.white,
+              child: Align(
+                alignment: Alignment.center,
+                child: Text("JEM018"),
+              )
+          ), top: 330, left: 20),
+          Positioned(child: Text("Lista de Productos"), top: 370)
         ],
       ),
-      actions: <Widget>[
-        new FlatButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          textColor: Theme.of(context).primaryColor,
-          child: const Text('Close'),
-        ),
-      ],
     );
   }
 }
